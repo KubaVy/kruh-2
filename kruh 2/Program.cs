c260b0237bf1b46eda60834b0216ab04dce8f30c
Jakub Vykydal <Vykydal.Jakub@stud.vassboskovice.cz>
2021-11-02 10:17:44 +0100

    Add project files.

---
+++ kruh 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace kruh_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double polomer;
+            double prumer;
+            Console.WriteLine();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+    }
+}
